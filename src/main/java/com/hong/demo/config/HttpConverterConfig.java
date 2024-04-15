f0909c895b9cccd9d5f3815a786f318d4746439c
hong le <vuanhde@yahoo.de>
2024-04-15 20:49:44 +0200

    6.commit am 15.04.24

--- src/main/java/com/hong/demo/config/HttpConverterConfig.java
+++ src/main/java/com/hong/demo/config/HttpConverterConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+// import org.springframework.http.converter.HttpMessageConverter;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
@@ -14,41 +15,54 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.time.format.DateTimeFormatter;
 
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+
 
 @Configuration
 public class HttpConverterConfig {
 
+    // public static final String DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
     public static final String DATETIME_FORMAT = "dd-MM-yyyy HH:mm";
 
-    @Primary
-    @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper objMapper = new ObjectMapper();
+    private Jackson2ObjectMapperBuilder getObjectMapperBuilder(){
+        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+        builder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATETIME_FORMAT)));
+        builder.serializationInclusion(JsonInclude.Include.NON_NULL);
+        return builder;
+    }
 
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper() {
+        
         JavaTimeModule module = new JavaTimeModule();
         module.addSerializer(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATETIME_FORMAT)));
 
-        objMapper
-        .setSerializationInclusion(JsonInclude.Include.NON_NULL)
-        .registerModule(module);
+        ObjectMapper objMapper = new ObjectMapper();
+        objMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        objMapper.registerModule(module);
 
-        return objMapper;    
+        return objMapper; 
+
+        // return getObjectMapperBuilder().build();
     }
 
     // @Bean
     // public Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
-    //     Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
-    //     builder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATETIME_FORMAT)));
-    //     builder.serializationInclusion(JsonInclude.Include.NON_NULL);
-    //     return builder;
+    //     return getObjectMapperBuilder();
     // }
 
     // @Bean
     // public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
-    //     Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
-    //     builder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATETIME_FORMAT)));
-    //     builder.serializationInclusion(JsonInclude.Include.NON_NULL);
-    //     return new MappingJackson2HttpMessageConverter(builder.build());
+    //     ObjectMapper objMapper = getObjectMapperBuilder().build();
+    //     return new MappingJackson2HttpMessageConverter(objMapper);
     // }
+
+    // @Bean
+    // public HttpMessageConverters customConverters() {
+    //     ObjectMapper objMapper = getObjectMapperBuilder().build();
+    //     return new HttpMessageConverters(new MappingJackson2HttpMessageConverter(objMapper));
+    // }
+
 }
 
