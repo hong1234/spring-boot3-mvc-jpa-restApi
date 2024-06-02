94991a21746a3b4ced2f4c42cb5503d127aad6c2
hong le <vuanhde@yahoo.de>
2024-06-02 13:33:36 +0200

    17.commit am 02.06.24

--- src/test/java/com/hong/demo/MvcJpaRestApiHApplicationTests.java
+++ src/test/java/com/hong/demo/MvcJpaRestApiHApplicationTests.java
@@ -1,9 +1,9 @@
 package com.hong.demo;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
-// import java.util.Date;
-import java.time.LocalDateTime;
+// import java.lang.String;
 
 import org.junit.jupiter.api.Test;
 import org.apache.commons.codec.binary.Base64;
@@ -28,8 +28,26 @@ import org.springframework.http.HttpStatus;
 import com.hong.demo.domain.Book;
 import com.hong.demo.exceptions.ErrorDetails;
 
+// import org.skyscreamer.jsonassert.JSONAssert;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import java.io.IOException;
+import org.json.JSONObject;
+import org.json.JSONException;
+
+// @SpringBootTest
+// class MvcJdbcRestApiHApplicationTests {
+
+// 	@Test
+// 	void contextLoads() {
+// 	}
+
+// }
+
+
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-class MvcJpaRestApiHApplicationTests { 
+class MvcJpaRestApiHApplicationTests {
 
 	@LocalServerPort
 	private int port;
@@ -37,79 +55,103 @@ class MvcJpaRestApiHApplicationTests {
 	@Autowired
 	private TestRestTemplate restTemplate;
 
+	// @Autowired
+	// private ObjectMapper objectMapper;
+
 	/*
     * Add HTTP Authorization header, using Basic-Authentication to send user-credentials.
     */
-	private static HttpHeaders getHeaders(){
-    	String plainCredentials="admin:admin";
+	private static HttpHeaders getHeaders2(){   // header for Basic-Authentication
+    	String plainCredentials= "autor:autor"; // name:password
     	String base64Credentials = new String(Base64.encodeBase64(plainCredentials.getBytes()));
     	
     	HttpHeaders headers = new HttpHeaders();
-        //headers.setContentType(MediaType.APPLICATION_JSON);
-    	headers.add("Authorization", "Basic " + base64Credentials);
+        
+    	// headers.add("Authorization", "Basic " + base64Credentials);
+		headers.set("Authorization", "Basic " + base64Credentials);
     	headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		headers.setContentType(MediaType.APPLICATION_JSON);
     	return headers;
     }
-	
+
+	private HttpHeaders getHeaders(){
+		HttpHeaders headers = new HttpHeaders();
+		headers.setBasicAuth("autor", "autor"); // name:password
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));		
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		return headers;
+	}
+
 	private String getRootUrl(){
 		return "http://localhost:"+port;
 	}
-	
+
+	@Test
+	public void testCreateBook(){
+
+		Book book = new Book();
+		book.setTitle("Exploring SpringBoot3");
+		book.setContent("SpringBoot 3 is awesome!");
+		book.setCreatedOn(null);
+
+		// HttpHeaders requestHeaders = getHeaders2();
+		// HttpEntity<Object> request = new HttpEntity<Object>(book, requestHeaders);
+
+		HttpHeaders requestHeaders = getHeaders();
+		HttpEntity<Book> request = new HttpEntity<Book>(book, requestHeaders);
+
+		ResponseEntity<String> response = restTemplate.exchange(
+			getRootUrl()+"/api/books", 
+			HttpMethod.POST, 
+			request, 
+			String.class
+		);
+
+		// System.out.println(response.getBody());
+		assertThat(response.getStatusCodeValue()).isEqualTo(201);
+
+		try {
+			// JSON to Java mapping code
+			// Book book2 = objectMapper.readValue(response.getBody(), Book.class);
+			// assertThat(book2.getTitle()).isEqualTo("Exploring SpringBoot3");
+
+			// JSONObject jo = new JSONObject("{\"name\": \"John----\"}"); // 
+			// System.out.println(jo.getString("name"));
+
+			JSONObject jo = new JSONObject(response.getBody());
+			assertThat(jo.getString("title")).isEqualTo("Exploring SpringBoot3");
+
+		} catch (JSONException e) {
+			System.out.println("Error occurred during JSON parsing: " + e.getMessage());
+		} 
+		// catch (Exception e) {
+		// 	System.out.println("Exception occurred: " + e.getMessage());
+		// }
+
+		
+	}
+
 	@Test
 	public void testGetAllBooks(){	
-        HttpEntity<String> request = new HttpEntity<String>(getHeaders());
-        ResponseEntity<Book[]> response = restTemplate.exchange(getRootUrl()+"/api/books", HttpMethod.GET, request, Book[].class);
-        // List<Book> books = Arrays.asList(response.getBody());
-        // assertNotNull(books);
+		HttpHeaders headers = getHeaders();
+		HttpEntity<String> entity = new HttpEntity<>(null, headers);
+		ResponseEntity<String> response = restTemplate.exchange(
+			getRootUrl()+"/api/books", 
+			HttpMethod.GET, 
+			entity, 
+			String.class
+		);
 		assertThat(response.getStatusCodeValue()).isEqualTo(200);
 		assertThat(response.getBody()).hasSizeGreaterThan(0);
 	}
-        
-   
+
 	// @Test
-	// public void testGetBookById(){
-	// 	HttpEntity<String> request = new HttpEntity<String>(getHeaders());
-    //     ResponseEntity<Book> response = restTemplate.exchange(getRootUrl()+"/api/books/1", HttpMethod.GET, request, Book.class);
-    //     assertNotNull(response.getBody());
+	// public void testDeleteBook(){
+	// 	String bookId = "5";
+	// 	HttpHeaders requestHeaders = getHeaders();
+	// 	HttpEntity<String> request = new HttpEntity<>(requestHeaders);
+	// 	ResponseEntity<ErrorDetails> responseEntity = restTemplate.exchange(getRootUrl()+"/api/books/"+bookId, HttpMethod.DELETE, request, ErrorDetails.class);
+	// 	assertThat(responseEntity.getStatusCodeValue()).isEqualTo(200);
 	// }
-
-       
-	@Test
-	public void testCreateBook(){
-		Book book = new Book();
-		book.setTitle("Exploring SpringBoot REST");
-		book.setContent("SpringBoot is awesome!!");
-		book.setCreatedOn(LocalDateTime.now());
-		
-        HttpEntity<Object> request = new HttpEntity<Object>(book, getHeaders());
-        ResponseEntity<Book> bookResponse = restTemplate.exchange(getRootUrl()+"/api/books", HttpMethod.POST, request, Book.class);
-        assertThat(bookResponse.getStatusCodeValue()).isEqualTo(200);
-		// assertNotNull(bookResponse);
-        // assertNotNull(bookResponse.getBody());	
-	}
-
-	
-	// @Test
-	// public void testUpdateBook(){
-	// 	int id = 1;
-    //     Book book = new Book();
-    //     book.setId(id);
-    //     book.setTitle("UPDATE");
-	// 	book.setContent("UPDATE");
-	// 	book.setCreatedOn(LocalDateTime.now());
-
-    //     HttpEntity<Object> request = new HttpEntity<Object>(book, getHeaders());
-	// 	ResponseEntity<Book> response = restTemplate.exchange(getRootUrl()+"/api/books/"+id, HttpMethod.PUT, request, Book.class);
-    //     assertNotNull(response.getBody());
-	// }
-        
-	@Test
-	public void testDeleteBook(){
-		int id = 2;
-        HttpEntity<String> request = new HttpEntity<String>(getHeaders());
-        ResponseEntity<String> response = restTemplate.exchange(getRootUrl()+"/api/books/"+id, HttpMethod.DELETE, request, String.class);
-        // assertNull(response.getBody());
-		assertThat(response.getBody()).hasSize(0); //.hasSize(0);
-	}
 
 }
